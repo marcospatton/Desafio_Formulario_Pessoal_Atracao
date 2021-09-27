e847a3f82ba3e06c14df3c4a16daad4dcb4f1168
Marco Antonio da Silva Francisco <marcospatton@hotmail.com>
2021-09-26 23:21:25 -0300

    adicionando js cpf

---
+++ js/scriptCpf.js
@@ -0,0 +1,8 @@
+function mascara_cpf(){
+    var cpf= document.getElementById('cpf')
+    if(cpf.value.length == 3 || cpf.value.length == 7){
+        cpf.value = cpf.value += "."
+    } else if(cpf.value.length == 11){
+        cpf.value = cpf.value += "-"
+    }
+}
